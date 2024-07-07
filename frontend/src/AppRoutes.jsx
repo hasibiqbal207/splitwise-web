9fdf93346fa6741d71a859a1dc05db129d9be2da
hasibiqbal207 <hasibiqbal207@gmail.com>
2024-07-07 17:34:26 +0200

    Feat: Group dashboard, create, edit component added.

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -5,10 +5,14 @@ import {
   Route,
   // Navigate,
 } from "react-router-dom";
+import configData from "./config/config.json";
 
 import Login from "./components/login/Login.jsx";
 import Registration from "./components/registration/Registration.jsx";
 import Profile from "./components/profile";
+import Group from "./components/groups";
+import CreateGroup from "./components/groups/CreateGroup.jsx";
+import ViewGroup from "./components/groups/viewGroup/index.jsx";
 
 const AppRoutes = () => {
   return (
@@ -17,6 +21,10 @@ const AppRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/dashboard/userProfile" element={<Profile />} />
+        <Route path={configData.USER_GROUPS_URL} element={<Group />} />
+        <Route path={configData.CREATE_GROUP_URL} element={<CreateGroup />} />
+        <Route path={configData.VIEW_GROUP_ROUTER_URL} element={<ViewGroup />} />
+      
       </Routes>
     </Router>
   );
