3ea3c0c39a87af9ab111c61e6d3748e541499486
Hasib Iqbal <hasibiqbal207@gmail.com>
2025-08-23 04:20:46 +0200

    updated docker configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 6004,
+    port: 6002,
     host: '0.0.0.0',
   },
   base: '/',
